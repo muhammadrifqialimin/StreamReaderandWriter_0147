33387f70b2fd8699d26f82e075a70c80231016ad
Muhammad Rifqi Alimin <rifqi.alimin.ft23@mail.umy.ac.id>
2025-06-10 18:33:25 +0700

    penanda 1 dan pesan array integer 3 elemen

--- exeptionStandard.cpp
+++ exeptionStandard.cpp
@@ -2,3 +2,12 @@
 #include <exception>
 #include <array>
 using namespace std;
+
+int main()
+{
+    cout << "Awal Program" << endl;
+    try {
+        array<int, 3> data = { 10, 20, 30 };
+        cout<<data.at(5)<<endl;
+    }
+}
