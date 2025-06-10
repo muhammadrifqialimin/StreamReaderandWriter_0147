037400c2b921861cd67511084e97e3a5045093eb
Muhammad Rifqi Alimin <rifqi.alimin.ft23@mail.umy.ac.id>
2025-06-10 17:28:07 +0700

    memanggil file

--- ioFile.cpp
+++ ioFile.cpp
@@ -5,4 +5,10 @@ using namespace std;
 
 int main() {
     string baris;
+    ofstream outfile;
+    outfile.open("contohfile.txt");
+    cout << ">= menulis file, \'q\' untuk keluar" << endl;
+    while (true) {
+        cout << ""
+    }
 }
