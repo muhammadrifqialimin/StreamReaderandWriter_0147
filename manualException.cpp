b5df3c318f02fa503f3ae9de3e6d4500da3b6f01
Muhammad Rifqi Alimin <rifqi.alimin.ft23@mail.umy.ac.id>
2025-06-10 18:47:24 +0700

    blok akan dieksekusi

---
+++ manualException.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    try {
+        cout << "Selamat Belajar di Prodi TI UMY" << endl;
+        throw 0.5;
+        cout << "Pernyataan tidak akan dieksekusi" << endl;
+    }
+    catch (int a) {
+        cout << "Pengecualian akan dieksekusi" << endl;
+    }
+    catch (...) {
+    }
+}
